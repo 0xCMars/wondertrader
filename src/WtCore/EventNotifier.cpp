4e89e4f23e17944a5a496135819c86adcf1746ce
MarsChenSE <MarsChenSE@gmail.com>
2025-07-21 11:47:56 +0800

    [ADD] include file

--- src/WtCore/EventNotifier.cpp
+++ src/WtCore/EventNotifier.cpp
@@ -14,6 +14,7 @@
 #include "../Share/DLLHelper.hpp"
 
 #include "../Includes/WTSTradeDef.hpp"
+#include "../Includes/WTSDataDef.hpp"
 #include "../Includes/WTSCollection.hpp"
 #include "../Includes/WTSVariant.hpp"
 
