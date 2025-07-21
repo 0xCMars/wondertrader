696afee0581982f2c1d3b68108379169af6ceaa2
MarsChenSE <MarsChenSE@gmail.com>
2025-07-22 00:15:12 +0800

    [FIX]

--- src/WtCore/EventNotifier.cpp
+++ src/WtCore/EventNotifier.cpp
@@ -423,7 +423,7 @@ void EventNotifier::notify_tick(const char* stdCode, WTSTickData* newTick)
 	if (newTick == NULL || _mq_sid == 0)
 		return;
 	WTSLogger::debug("notify_tick");
-	WTSTickData* tick = WTSTickData::create(*newTick);
+	WTSTickData* tick = WTSTickData::create(newTick->_data);
 	std::string strCode = stdCode;
 	tick->retain();
 	_asyncio.post([this, strCode, tick]() {
